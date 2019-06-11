0475ed2f23a779982d33cf4b392569e75132baea
kashsid <kashifsiddique@gmail.com>
2019-06-11 11:22:31 -0500

    add app

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+
+
+import React, { Component } from 'react';
+import Calculator from '../Calculator/Calculator';
+import './App.css';
+
+
+const App = () => (
+    <div className="App">
+        <header>
+            <h1>Code Challenge Calculator</h1>
+        </header>
+        <Calculator />
+    </div>
+);
+
+export default App;
